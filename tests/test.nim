61a415bbed84d0220cf32225836556f6a1fa1205
John <goldnotch@mail.ru>
2021-12-12 10:13:20 +0500

    Update test.nim

--- tests/test.nim
+++ tests/test.nim
@@ -1,7 +1,7 @@
 # Copyright 2019, NimGL contributors.
 
 import unittest
-import glfw, glfw_os
+import glfw, glfw_native
 
 proc keyProc(window: GLFWWindow, key: int32, scancode: int32, action: int32, mods: int32): void {.cdecl.} =
   if key == GLFWKey.Escape and action == GLFWPress:
